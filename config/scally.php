28b2b658f1e8e37a6e4e3be250c1130ffb43f93b
Malykhin Sergey <malikin.sergey.1988@gmail.com>
2021-01-25 13:08:13 +0300

    blueprints 3

--- config/scally.php
+++ config/scally.php
@@ -1,52 +1,70 @@
 <?php
 
 return [
-    
+
     /*
      * If 'stubs_path' is null, default stubs, included in package, will be used.
      * Use php artisan vendor:publish --tag=scally_stubs to publish stubs to resources directory and customize them.
      */
-    
+
     'stubs_path' => null,
-    
+
     /*
      * Scaffold path must be writeable for webserver.
      */
-    
+
     'scaffold_path' => storage_path('scaffold'),
-    
+
     'admin_view_namespace' => 'dashboard',
-    
+
     'admin_route_namespace' => 'dashboard',
-    
+
     'models_path' => 'app/Models',
-    
+
     'controllers_path' => 'app/Http/Controllers/Dashboard',
-    
+
     'controller_namespace' => app()->getNamespace() . 'Http\Controllers\Dashboard',
-    
-    'model_namespace' => app()->getNamespace().'Models',
-    
+
+    'model_namespace' => app()->getNamespace() . 'Models',
+
     'locale' => app()->getLocale(),
-    
+
     'attribute_types' => [
-        
+
         'string' => [
             'name' => 'String',
             'blueprint' => 'string',
-        
+
         ],
-        
+
         'text' => [
             'name' => 'Text',
             'blueprint' => 'text',
-        
+
         ],
-        
+
+        'unsigned_integer' => [
+            'name' => 'Unsigned Int',
+            'blueprint' => 'unsignedInteger',
+
+        ],
+
         'integer' => [
+            'name' => 'Int',
+            'blueprint' => 'integer',
+
+        ],
+
+        'unsigned_big_integer' => [
             'name' => 'Unsigned BigInt',
             'blueprint' => 'unsignedBigInteger',
-        
+
+        ],
+
+        'big_integer' => [
+            'name' => 'BigInt',
+            'blueprint' => 'bigInteger',
+
         ],
 
         'unsigned_decimal' => [
@@ -59,12 +77,12 @@ return [
             'name' => 'Decimal',
             'blueprint' => 'decimal',
         ],
-        
+
         'variants' => [
             'name' => 'Variants (in DB: string)',
             'blueprint' => 'string',
         ],
-          
+
         'jsonb' => [
             'name' => 'JSONB',
             'blueprint' => 'jsonb',
---
+++ stubs/views/types/unsigned_decimal.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+    {!! Form::label('AttrName', 'AttrTitle') !!}
+    {!! Form::text('AttrName', old('AttrName'), ['class' => 'form-control', 'placeholder' => 'AttrTitle']) !!}
+</div>
